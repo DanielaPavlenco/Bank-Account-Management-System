a4130fc4e974ec5ff20bb5959785e0a3f38b7941
DanielaPavlenco <101560755+DanielaPavlenco@users.noreply.github.com>
2023-10-28 17:43:50 +0300

    Update BankAccount.hpp

--- T1/src/BankAccount.hpp
+++ T1/src/BankAccount.hpp
@@ -13,13 +13,13 @@ public:
     // Constructor pentru a inițializa contul bancar
     BankAccount(const std::string& accNumber, const std::string& accHolder, double initialBalance);
 
-    // Cerința 2: Încapsulare - Metode pentru a accesa datele private
+    // Încapsulare - Metode pentru a accesa datele private
     const std::string& getAccountNumber() const;
     const std::string& getAccountHolder() const;
     double getBalance() const;
 
-    // Cerința 3: Inițializarea membrilor din constructor
-    // Cerința 4: Eliberarea heap-ului în destructor
+    // Inițializarea membrilor din constructor
+    //  Eliberarea heap-ului în destructor
     ~BankAccount();
 
     // Cerința 5: Suprascrierea copy constructorului
