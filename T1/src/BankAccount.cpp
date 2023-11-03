f43997e476bd44808b760e1539ef5ece4cba700b
DanielaPavlenco <101560755+DanielaPavlenco@users.noreply.github.com>
2023-11-03 22:12:04 +0200

    Update BankAccount.cpp

--- T1/src/BankAccount.cpp
+++ T1/src/BankAccount.cpp
@@ -1,99 +1,75 @@
 #include "BankAccount.hpp"
 #include <iostream>
-using namespace std;
 
-// Constructor pentru a inițializa contul bancar
-BankAccount::BankAccount(const string& accNumber, const string& accHolder, double initialBalance) {
-    accountNumber = accNumber;
-    accountHolder = accHolder;
-    balance = new double(initialBalance);
-}
+namespace BankAccounts {
 
-// Incapsulare - Metode pentru a accesa datele private
-const string& BankAccount::getAccountNumber() const {
-    return accountNumber;
-}
-
-const string& BankAccount::getAccountHolder() const {
-    return accountHolder;
-}
-
-double BankAccount::getBalance() const {
-    return *balance;
-}
-
-//Eliberarea heap-ului în destructor
-BankAccount::~BankAccount() {
-    delete balance;
-}
-
-//Suprascrierea copy constructorului
-BankAccount::BankAccount(const BankAccount& other) {
-    accountNumber = other.accountNumber;
-    accountHolder = other.accountHolder;
-    balance = new double(*(other.balance));
-}
-
-//Suprascrierea operatorului de atribuire
-BankAccount& BankAccount::operator=(const BankAccount& other) {
-    if(this==&other) {
-        return *this; //Evitam auto-atribuirea
+    // Constructorul clasei BankAccount
+    BankAccount::BankAccount(const std::string& accNumber, const std::string& accHolder, double initialBalance)
+        : accountNumber(accNumber), accountHolder(accHolder) {
+        balance = new double(initialBalance);
     }
-    accountNumber = other.accountNumber;
-    accountHolder = other.accountHolder;
 
-
-    //Eliberaram memoria veche din heap si alocam memorie noua pentru balance
-    delete balance;
-    balance = new double(*(other.balance));
-
-    return *this;
-}
-
-// Metodă pentru a efectua un depozit în cont
-void BankAccount::deposit(double amount) {
-    if (amount > 0) {
-        *balance += amount;
-        cout << "Deposit amount " << amount << " RON done. The actual balance is " << *balance << " RON." << endl;
-    } else {
-        cout << "The deposit amount should be more than zero." << endl;
+    // Destructorul clasei BankAccount
+    BankAccount::~BankAccount() {
+        delete balance;
     }
-}
 
-// Metodă pentru a efectua o retragere din cont
-void BankAccount::withdraw(double amount) {
-    if (amount > 0 && amount <= *balance) {
-        *balance -= amount;
-        cout << "Withdrawal amount of " << amount << " RON done. The actual balance is " << *balance << " RON." << endl;
-    } else if (amount > *balance) {
-        cout << "Insufficient funds for the withdrawal." << endl;
-    } else {
-        cout << "The withdrawal amount should be more than zero." << endl;
+    // Metoda pentru a obține numărul de cont
+    const std::string& BankAccount::getAccountNumber() const {
+        return accountNumber;
     }
-}
 
-// Metodă pentru a afișa informațiile despre cont
-void BankAccount::displayAccountInfo() {
-    cout << "Account number: " << accountNumber << endl;
-    cout << "Account Holder: " << accountHolder << endl;
-    cout << "Actual balance: " << *balance << " RON" << endl;
-}
+    // Metoda pentru a obține titularul contului
+    const std::string& BankAccount::getAccountHolder() const {
+        return accountHolder;
+    }
 
-int main() {
-    // Create a BankAccount object
-    BankAccount myAccount("242546", "Emily Swift", 1000.0);
+      // Metoda pentru a obține soldul contului
+    double BankAccount::getBalance() const {
+        return *balance;
+    }
+ 
+    void BankAccount::setAccountNumber(const std::string& accNumber) {
+        accountNumber = accNumber;
+    }
 
-    // Display initial account information
-    cout << "Initial account information:" << endl;
-    myAccount.displayAccountInfo();
+    void BankAccount::setAccountHolder(const std::string& accHolder) {
+        accountHolder = accHolder;
+    }
 
-    // Perform some transactions
-    myAccount.deposit(700.0);
-    myAccount.withdraw(200.0);
+     // Implementarea constructorului de mutare (move constructor) pentru clasa BankAccount
+    BankAccount::BankAccount(BankAccount&& other) noexcept 
+    : accountNumber(std::move(other.accountNumber)),
+    accountHolder(std::move(other.accountHolder)), balance(other.balance) {
+    other.balance = nullptr; // Eliberăm resursele din obiectul sursă (other)
+    }
 
-    // Display updated account information
-    cout << "\nUpdated account information:" << endl;
-    myAccount.displayAccountInfo();
+    void BankAccount::deposit(double amount) {
+        if (amount > 0) {
+            *balance += amount;
+            std::cout << "Deposit amount " << amount << " RON done. The actual balance is " << *balance << " RON." << std::endl;
+        } else {
+            std::cout << "The deposit amount should be more than zero." << std::endl;
+        }
+    }
 
-    return 0;
+    // Metoda pentru a efectua o retragere din cont
+    void BankAccount::withdraw(double amount) {
+        if (amount > 0 && amount <= *balance) {
+            *balance -= amount;
+            std::cout << "Withdrawal amount of " << amount << " RON done. The actual balance is " << *balance << " RON." << std::endl;
+        } else if (amount > *balance) {
+            std::cout << "Insufficient funds for the withdrawal." << std::endl;
+        } else {
+            std::cout << "The withdrawal amount should be more than zero." << std::endl;
+        }
+    }
+
+    // Metoda pentru a afișa informațiile despre cont
+    void BankAccount::displayAccountInfo() const {
+        std::cout << "Account number: " << accountNumber << std::endl;
+        std::cout << "Account Holder: " << accountHolder << std::endl;
+        std::cout << "Actual balance: " << *balance << " RON" << std::endl;
+    }
+
 }
